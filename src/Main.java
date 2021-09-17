a29c148f81644eaf3e3b19adf1b5a06046700285
Ivanova Irina <ivanovairishav@ya.ru>
2021-09-17 13:35:04 +0300

    add HomeWork2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,25 +37,48 @@ public class Main {
 
         String result = sb.toString();
 
-//        String result = "Логи" + "\n" + creatingLogsForDirectories(src) +
-//                creatingLogsForDirectories(res) +
-//                creatingLogsForDirectories(savegames) +
-//                creatingLogsForDirectories(temp) +
-//                creatingLogsForDirectories(main) +
-//                creatingLogsForDirectories(test) +
-//                creatingLogsForDirectories(drawables) +
-//                creatingLogsForDirectories(vectors) +
-//                creatingLogsForDirectories(icons) +
-//                creatingLogsForFiles(mainJava) +
-//                creatingLogsForFiles(utilJava) +
-//                creatingLogsForFiles(tempTxt);
-//        System.out.println(result);
-
         try (FileWriter writer = new FileWriter("M://Games//temp//temp.txt", false)) {
             writer.write(result);
             writer.flush();
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }
+
+        GameProgress progress1 = new GameProgress(140, 50, 12, 105.5);
+        GameProgress progress2 = new GameProgress(200, 100, 50, 300.2);
+        GameProgress progress3 = new GameProgress(370, 120, 70, 367.0);
+
+        saveGame("M://Games//savegames//save1.dat", progress1);
+        saveGame("M://Games//savegames/save2.dat", progress2);
+        saveGame("M://Games//savegames/save3.dat", progress3);
+
+        zipFiles("M://Games//savegames//save.zip","M://Games//savegames//save1.dat");
+        zipFiles("M://Games//savegames//save.zip","M://Games//savegames//save2.dat");
+        zipFiles("M://Games//savegames//save.zip","M://Games//savegames//save3.dat");
+
+    }
+
+    private static void saveGame(String fileName, GameProgress progress) {
+        try (FileOutputStream fos = new FileOutputStream(fileName);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(progress);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void zipFiles(String zipName, String fileName) {
+        try (ZipOutputStream zout = new ZipOutputStream(new
+                FileOutputStream(zipName));
+             FileInputStream fis = new FileInputStream(fileName)) {
+            ZipEntry entry = new ZipEntry(fileName);
+            zout.putNextEntry(entry);
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            zout.write(buffer);
+            zout.closeEntry();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
 
     }
